0d952b7c173e8afde4b62916ce8295779b66a816
Ramkumar.G <76491314+ramkumar0718@users.noreply.github.com>
2025-06-20 20:11:49 +0530

    version.1 change port

--- website/settings.py
+++ website/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'USER': os.environ.get("USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
         'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT"),
+        'PORT': "5432",
     }
 }
 
